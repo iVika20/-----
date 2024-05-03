d51e9fc282bd17434d25dab780aabb6e6ece912a
Vika <igonina.v.d10@gmail.com>
2024-05-03 22:35:48 +0300

    1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pic = image.load('free-icon-tennis-ball-5140646.png')
 display.set_icon(pic)
 display.set_caption('ping-pong')
 
-mode = 0
+mode = 3
 background = transform.scale(image.load('tennis_court_background.jpg'), (800,500))
 image_player = transform.scale(image.load('free-icon-flying-saucer-11617509.png'), (200, 100))
 image_player = transform.rotate(image_player, 90)
@@ -65,10 +65,19 @@ class Ball(GameSprite):
         
 label1 = font.Font(None, 20)
 label2 = font.Font(None, 50)
+label3 = font.Font(None, 30)
 n1 = label1.render('ИГРОК 1', True, (255, 255, 255))
 n2 = label1.render('ИГРОК 2', True, (255, 255, 255))
 lab1 = label2.render('Победил ИГРОК 1', True, (240, 30, 200))
 lab2 = label2.render('Победил ИГРОК 2', True, (240, 30, 200))
+lab3 = label3.render('Управление ИГРОКОМ 1:', True, (255, 255, 255))
+lab4 = label3.render('для перемещения вверх нажмите клавишу "W"', True, (255, 255, 255))
+lab5 = label3.render('для перемещения вниз нажмите клавижу "S"', True, (255, 255, 255))
+lab6 = label3.render('Управление ИГРОКОМ 2:', True, (255, 255, 255))
+lab7 = label3.render('для перемещения вверх нажмите стрелку вверх', True, (255, 255, 255))
+lab8 = label3.render('для перемещения вниз нажмите стрелку вниз', True, (255, 255, 255))
+lab9 = label3.render('Чтобы НАЧАТЬ ИГРУ нажмите "O"', True, (255, 255, 255))
+
 
 player1 = Player(image_player, 0.7, 50, 150)
 player2 = Player(image_player_l, 0.7, 650, 150)
@@ -81,6 +90,17 @@ while game:
         if e.type == QUIT:
             game = False
 
+    if mode == 3: 
+        window.blit(lab3, (50, 50))
+        window.blit(lab4, (50, 90))
+        window.blit(lab5, (50, 130))
+        window.blit(lab6, (250, 190))
+        window.blit(lab7, (250, 230))
+        window.blit(lab8, (250, 270)) 
+        window.blit(lab9, (150, 350)) 
+        keys = key.get_pressed()
+        if keys[K_o]: mode = 0                 
+
     if mode == 0:
         window.blit(background, (0, 0))
         ball.reset()
@@ -88,7 +108,7 @@ while game:
         player2.reset()
         window.blit(n1, (10, 5))
         window.blit(n2, (700, 5))
-
+        
         ball.move()
         player1.control_move_l()
         player2.control_move()
